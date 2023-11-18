513dee2b21b1f691ac57d691919a5ee68312e0a3
Guido Gauna <guidogauna9@gmail.com>
2023-11-18 16:43:48 -0300

    ""

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var express = require('express');
-var exphbs  = require('express-handlebars');
+var { engine } = require('express-handlebars');
 var port = process.env.PORT || 3000
 
 var app = express();
- 
-app.engine('handlebars', exphbs());
+
+app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 
 app.use(express.static('assets'));
- 
+
 app.use('/assets', express.static(__dirname + '/assets'));
 
 app.get('/', function (req, res) {
